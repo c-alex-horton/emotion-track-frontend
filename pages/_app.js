b17f7c2560fcc794c4579f9d9944feaad4358504
Caleb Horton <c.alex.horton@gmail.com>
2022-09-16 11:54:04 -0500

    Normalize CSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
 import { UserProvider } from '../context/UserContext'
+import { Normalize } from 'styled-normalize'
 
 function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
+      <Normalize />
       <Component {...pageProps} />
     </UserProvider>)
 }
