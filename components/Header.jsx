d956e16bebea12fae6121d63509a6991fed7e52e
Caleb Horton <c.alex.horton@gmail.com>
2022-09-19 13:52:57 -0500

    Added register page

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import styled from 'styled-components'
 import { UserContext } from '../context/UserContext'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const HeaderStyles = styled.header`
   display: flex;
@@ -14,7 +15,21 @@ export default function Header() {
   return (
     <HeaderStyles>
       <h1>Emotion Tracker</h1>
-      {router.pathname !== '/' && <button onClick={logout}>Logout</button>}
+      {router.pathname === '/dashboard' && (
+        <button onClick={(e) => logout('Youve been logged out, yo')}>
+          Logout
+        </button>
+      )}
+      {router.pathname === '/' && (
+        <Link href='/register'>
+          <a>Sign Up</a>
+        </Link>
+      )}
+      {router.pathname === '/register' && (
+        <Link href='/'>
+          <a>Sign In</a>
+        </Link>
+      )}
     </HeaderStyles>
   )
 }
---
+++ pages/register.js
@@ -0,0 +1,99 @@
+import Head from 'next/head'
+import styled from 'styled-components'
+import Layout from '../components/Layout'
+import { useState, useContext } from 'react'
+import Router, { useRouter } from 'next/router'
+import { UserContext } from '../context/UserContext'
+
+const LoginStyles = styled.div`
+  display: flex;
+  form {
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+  }
+  justify-content: center;
+  flex-direction: column;
+  .message {
+    color: #d30000;
+    text-align: center;
+  }
+`
+
+export default function Home() {
+  const { user, setUser, message } = useContext(UserContext)
+
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`${process.env.SERVER_URL}/auth/register`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': 'http://localhost:3000',
+          'Access-Allow-Methods': 'POST',
+          'Access-Control-Allow-Credentials': 'true',
+        },
+        credentials: 'include',
+        redirect: 'follow',
+        body: JSON.stringify({
+          "name": name,
+          "email": username,
+          "password": password
+        })
+      })
+
+      const data = await response.json()
+      console.log(data)
+      if (response.status === 200) {
+        setUser({
+          token: data.token,
+          name: data.name
+        })
+        Router.push("/dashboard")
+      }
+    } catch (err) {
+      console.log(err);
+    }
+
+  }
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+
+  return (
+    <Layout>
+      <Head>
+        <title>Emotion Tracker</title>
+        <meta name="description" content="Track your emotions" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <LoginStyles>
+        <p className="message">{message}</p>
+        <form className='login-form' onSubmit={(e) => handleSubmit(e)}>
+          <label>Name</label>
+          <input type="text" onChange={(e) => handleName(e)} />
+          <label>Email</label>
+          <input type="text" onChange={(e) => handleUsername(e)} />
+          <label>Password</label>
+          <input type="password" onChange={(e) => handlePassword(e)} />
+          <input type="submit" value={"Login"} />
+        </form>
+      </LoginStyles>
+
+    </Layout>
+  )
+}
