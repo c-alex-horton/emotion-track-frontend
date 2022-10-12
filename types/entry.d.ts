82ef64906390521bdcdf7305a42edd2a69a58032
Caleb Horton <c.alex.horton@gmail.com>
2022-10-12 12:03:47 -0500

    Added global Types

---
+++ types/entry.d.ts
@@ -0,0 +1,11 @@
+// Entry Type for user entries
+type Entry = {
+    event: string
+    symbol: string
+    description: string
+    emotion: string
+    notes: string
+    time: string
+    tags: string[]
+    starred: boolean
+  }
